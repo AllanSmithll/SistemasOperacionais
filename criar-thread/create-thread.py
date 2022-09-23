9f8be275f9b4d797d303fc345885f81ea0547748
AllaSmithll <allanammancio2004@gmail.com>
2022-09-23 19:02:31 -0300

    criando Thread

---
+++ criar-thread/create-thread.py
@@ -0,0 +1,26 @@
+#Atividade terminada em 23/09/2022
+from threading import Thread, get_ident
+
+
+class Somador(Thread):
+
+    def __init__(self, inicio, fim):
+        Thread.__init__(self)
+        self.inicio = inicio
+        self.fim = fim
+        self.somatorio = 0
+
+    def run(self):
+        for i in range(self.inicio, self.fim + 1):
+            self.somatorio += i
+            print("Thread-", get_ident())
+
+
+s1 = Somador(0, 10)
+s2 = Somador(11, 20)
+s1.start()
+s2.start()
+s1.join()
+s2.join()
+print("Allan Alves Amâncio", s1.somatorio)
+print("Allan do Gera", s2.somatorio)
